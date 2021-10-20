ada4ca6a896e0c814a9b30da301acaf4f04169af
andreanpradanaa <andreanpradana2001@gmail.com>
2021-10-21 00:13:36 +0700

    commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const connect = require("./database/connection");
 const cors = require("cors");
 require("dotenv").config({ path: "../config.env" });
-const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
@@ -22,6 +21,6 @@ app.use("/api/clients", require("./controller/clients"));
 app.use("/api/teams", require("./controller/teams"));
 app.use("/api/projects", require("./controller/projects"));
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
